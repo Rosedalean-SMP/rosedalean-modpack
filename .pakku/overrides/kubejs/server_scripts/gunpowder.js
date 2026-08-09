54aad49ce31b9537e885727995bb2b6507c99d1c
Salad <salad@bysalad.studio>
2026-08-09 04:43:26 +0200

    kubejs stuff

---
+++ .pakku/overrides/kubejs/server_scripts/gunpowder.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.recipes.create.mixing(
+        'minecraft:gunpowder',
+        [
+            '2x #minecraft:coals',
+            'minecraft:blaze_powder'
+        ]
+    )
+})
---
+++ .pakku/overrides/kubejs/server_scripts/remove_diamond_recipe.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(event => {
+event.remove({ input: 'minecraft:coal_block', output:'minecraft:diamond' })
+})
